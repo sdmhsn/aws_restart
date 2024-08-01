e93ef6dcd4a2ac57972b065ca34ce624ced62256
Saddam Husein <59458401+sdmhsn@users.noreply.github.com>
2024-08-01 21:58:08 +0800

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,29 @@
+# Exercise 1: Introducing the list data type
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0]) # Accessing a list by position
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange" # Changing the values in a list
+print(myFruitList)
+
+# Exercise 2: Introducing the tuple data type
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0]) # Accessing a tuple by position
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Exercise 3: Introducing the dictionary data type
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"]) # Accessing a dictionary by name
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ numeric-data.py
@@ -0,0 +1,26 @@
+print('Python has three numeric types: int, float, and complex')
+
+# Exercise 2: Introducing the int data type
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + ' is of the data type ' + str(type(myValue)))
+
+# Exercise 3: Introducing the float data type
+myValue = 3.12
+print(myValue)
+print(type(myValue))
+print(str(myValue) + ' is of the data type ' + str(type(myValue)))
+
+# Exercise 4: Introducing the complex data type
+myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + ' is of the data type ' + str(type(myValue)))
+
+# Exercise 5: Introducing the bool data type
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + ' is of the data type ' + str(type(myValue)))
+
